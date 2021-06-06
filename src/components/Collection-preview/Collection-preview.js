c173b1bdf77f0d31ace42e0428eab689392483f3
Ye Shi <zoe.shiye@gmail.com>
2021-06-07 08:16:07 +1000

    Add: collection-item component

--- src/components/Collection-preview/Collection-preview.js
+++ src/components/Collection-preview/Collection-preview.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import './collection-preview.styles.scss';
+import React from "react";
+import "./collection-preview.styles.scss";
 
-const CollectionPreview = ({title, items}) => {
+const CollectionPreview = ({ title, items }) => {
   return (
     <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
         {items
           .filter((item, index) => index < 4)
-          .map(item => (
-          <div key={item.id}>{item.name}</div>
-        ))}
+          .map((item) => (
+            <div key={item.id}>{item.name}</div>
+          ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default CollectionPreview;
