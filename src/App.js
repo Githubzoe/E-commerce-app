beb3bc32a5411210f69d5cedb03a656b29cbd936
Ye Shi <zoe.shiye@gmail.com>
2021-06-06 14:30:42 +1000

    Add: collection-preview component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import React from 'react';
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from './pages/Homepage/Homepage';
-
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
-
+import ShopPage from './pages/Shop/ShopPage';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
